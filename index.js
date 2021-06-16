f8fe900bc881c23393397fa7a28dde4f5778c44a
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-06-17 00:43:58 +0300

    Completed exercise 3.8

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan")
 
+morgan.token('content', (req) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'));
 app.use(express.json());
 
 let persons = [
