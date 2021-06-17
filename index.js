c974ce37286b4855bf5948757ea3d8acfe604c6d
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-06-17 21:29:24 +0300

    Added cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan")
+const cors = require('cors')
 
+app.use(cors())
 morgan.token('content', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'));
 app.use(express.json());
