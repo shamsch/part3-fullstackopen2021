10a972a4ea6559d20e183a3fa209e0625c02fa7e
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2021-06-17 21:52:20 +0300

    added git ignore

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.post("/api/persons", (request, response) => {
   response.json(newPerson);
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
